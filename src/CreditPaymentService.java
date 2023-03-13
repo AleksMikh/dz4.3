5f756ca18a7aaaf077c6b876c4a0c068360fa54a
Mikhail Alekseev <rai1gun@mail.ru>
2023-03-14 01:48:56 +0300

    gitignore changed

--- src/CreditPaymentService.java
+++
@@ -1,7 +0,0 @@
-public class CreditPaymentService {
-    public int calculate(int period, int amount, double i_rate) {
-        double coefficient = i_rate + (i_rate / (Math.pow(1 + i_rate, period) - 1));
-        double payment = coefficient * amount;
-        return (int) payment;
-    }
-}
